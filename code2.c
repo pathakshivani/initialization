c6ff044123757102fc7afe7f33f2ad5e7447ef6c
shivani21 <101668002+pathakshivani@users.noreply.github.com>
2022-10-02 06:47:47 -0700

    Add files via upload

---
+++ code2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int arr[10],i,  even=0,  odd=0;
+
+
+    printf("enter the integer--");
+    scanf("%d",&arr[i]);
+
+
+    if (arr[i]%2==0)
+    {
+        even=even+arr[i];
+
+        printf("even=%d\n",even);
+    }
+    else
+    {
+        odd=odd+arr[i];
+        printf("odd=%d\n",odd);
+    }
+
+
+
+
+    return 0;
+
+
+}
